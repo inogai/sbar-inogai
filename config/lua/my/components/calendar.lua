45b8e9e16e16713e0ed234e51d5678f522943a7b
inogai <inogai.sign@gmail.com>
2025-11-19 02:00:18 +0800

    feat(calendar): add secs

--- config/lua/my/components/calendar.lua
+++ config/lua/my/components/calendar.lua
@@ -3,12 +3,12 @@ local cal = sbar.add("item", "components.calendar", {
 		string = os.date("URA 99 UND 99:99"),
 	},
 	position = "right",
-	update_freq = 20,
+	update_freq = 1,
 	click_script = "open -a 'Calendar'",
 })
 
 cal:subscribe({ "forced", "routine", "system_woke" }, function(_)
 	cal:set({
-		label = os.date("%a %d %b %H:%M"),
+		label = os.date("%a %d %b %H:%M:%S"),
 	})
 end)
