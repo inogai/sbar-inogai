e8be0a01a457ac2f8da3de21829f2a0102b05642
inogai <inogai.sign@gmail.com>
2025-11-20 00:09:37 +0800

    feat(title): add max_length

--- config/lua/my/components/window_title.lua
+++ config/lua/my/components/window_title.lua
@@ -2,10 +2,16 @@ local M = sbar.add("item", "components.window.title", {
 	padding_left = 8,
 })
 
+local TITLE_MAX_LENGTH = 20
+
 local function update_window_title()
 	sbar.exec("aerospace list-windows --focused --json", function(output)
 		local window = output[1]
 		local title = window["window-title"] or ""
+
+		if title:len() > TITLE_MAX_LENGTH then
+			title = title:sub(1, TITLE_MAX_LENGTH - 2) .. "..."
+		end
 
 		M:set({ label = { string = title } })
 	end)
