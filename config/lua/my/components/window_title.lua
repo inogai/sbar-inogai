0b6b9cfa5e53b99fc610cb0582ea534a42987693
inogai <inogai.sign@gmail.com>
2025-11-20 00:21:53 +0800

    feat(title): add appname

--- config/lua/my/components/window_title.lua
+++ config/lua/my/components/window_title.lua
@@ -1,27 +1,34 @@
 local M = sbar.add("item", "components.window.title", {
 	padding_left = 8,
+	icon = { font = { style = "Bold" } },
 })
 
-local TITLE_MAX_LENGTH = 20
+local LABEL_MAX_LENGTH = 40
 
 local function update_window_title()
 	sbar.exec("aerospace list-windows --focused --json", function(output)
 		local window = output[1]
 		local title = window["window-title"] or ""
+		local app_name = window["app-name"] or ""
 
-		if title:len() > TITLE_MAX_LENGTH then
-			title = title:sub(1, TITLE_MAX_LENGTH - 2) .. "..."
+		local icon = "▎" .. app_name .. " @ "
+		local label = title
+
+		if label:len() > LABEL_MAX_LENGTH then
+			label = label:sub(1, LABEL_MAX_LENGTH - 2) .. "..."
 		end
 
-		M:set({ label = { string = title } })
+		M:set({
+			icon = { string = icon },
+			label = { string = label },
+		})
 	end)
 end
 
-M:subscribe("front_app_switched", function(_)
-	update_window_title()
-end)
-
-M:subscribe("aerospace_workspace_change", function(_)
+M:subscribe({
+	"front_app_switched",
+	"aerospace_workspace_change",
+}, function(_)
 	update_window_title()
 end)
 
